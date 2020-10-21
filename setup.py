3f0287c99cc2d4803a2dd7a3cd6805f5c5cfc099
Ilya Baldin <ibaldin@renci.org>
2020-10-21 16:50:20 -0400

    Fixing packaging per @kthare10 request

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setuptools.setup(
   url="https://github.com/fabric-testbed/InformationModel",
   long_description="FABRIC Information Model Library",
   long_description_content_type="text/plain",
+  packages=setuptools.find_packages(include=['fim']),
+  include_package_data=True,
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
