2178b95086781abb1db76bd57845a07cee322abe
Ilya Baldin <ibaldin@renci.org>
2020-12-21 13:59:34 -0500

    Upping dependencies to be compatible with 3.7/CF

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
   name="fabric_fim",
-  version="0.16",
+  version="0.17",
   author="Ilya Baldin, Komal Thareja",
   description="FABRIC Information Model Library",
   url="https://github.com/fabric-testbed/InformationModel",
