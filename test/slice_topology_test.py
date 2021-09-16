52f07e585bcd6e98152dcaef739f845537464589
Ilya Baldin <ibaldin@renci.org>
2021-09-16 14:01:16 -0400

    Adding tests for new capabilities and missed tests

--- test/slice_topology_test.py
+++ test/slice_topology_test.py
@@ -2,11 +2,24 @@ import unittest
 
 import fim.user as f
 
+from fim.graph.slices.neo4j_asm import Neo4jASM, Neo4jASMFactory
+from fim.graph.neo4j_property_graph import Neo4jGraphImporter
+
 
 class SliceTest(unittest.TestCase):
 
+    neo4j = {"url": "neo4j://0.0.0.0:7687",
+             "user": "neo4j",
+             "pass": "password",
+             "import_host_dir": "neo4j/imports/",
+             "import_dir": "/imports"}
+
     def setUp(self) -> None:
         self.topo = f.ExperimentTopology()
+        self.n4j_imp = Neo4jGraphImporter(url=self.neo4j["url"], user=self.neo4j["user"],
+                                          pswd=self.neo4j["pass"],
+                                          import_host_dir=self.neo4j["import_host_dir"],
+                                          import_dir=self.neo4j["import_dir"])
 
     def tearDown(self) -> None:
         pass
@@ -27,9 +40,15 @@ class SliceTest(unittest.TestCase):
         # properties checks
         n3.set_properties(details='some details')
         self.assertEqual(n3.get_property('details'), 'some details')
+        self.assertEqual(n3.details, 'some details')
+        n3.details = 'other details'
+        self.assertEqual(n3.details, 'other details')
 
         self.assertTrue('capacities' in n3.list_properties())
         self.assertEqual(n3.get_property('image_ref'), 'http://some.image')
+        self.assertEqual(n3.image_ref, 'http://some.image')
+        n3.image_ref = 'http://other.image'
+        self.assertEqual(n3.image_ref, 'http://other.image')
 
         # component checks
         n1.add_component(ctype=f.ComponentType.GPU, model='RTX6000', name='gpu1')
@@ -50,9 +69,13 @@ class SliceTest(unittest.TestCase):
         p1 = nic2.interfaces['nic2-p1']
         p1lab = p1.get_property('labels')
         self.assertEqual(p1lab.local_name, "p1")
+        p1lab = p1.labels
+        self.assertEqual(p1lab.local_name, "p1")
 
         p2 = nic2.interfaces['nic2-p2']
         p2lab = p2.get_property('labels')
+        self.assertEqual(p2lab.local_name, "p2")
+        p2lab = p2.labels
         self.assertEqual(p2lab.local_name, "p2")
         # check parent code
         assert(self.topo.get_parent_element(e=gpu1) == n1)
@@ -79,14 +102,17 @@ class SliceTest(unittest.TestCase):
         self.assertTrue(len(nic1.interfaces) == 1)
         cap = f.Capacities()
         cap.set_fields(bw=50, unit=1)
-        nic1.set_properties(capacities=cap)
+        #nic1.set_properties(capacities=cap)
+        nic1.capacities = cap
         lab = f.Labels()
         lab.set_fields(ipv4="192.168.1.12")
-        nic1.set_properties(labels=lab)
+        #nic1.set_properties(labels=lab)
+        nic1.labels = lab
         self.assertEqual(nic1.get_property('capacities').bw, 50)
         self.assertEqual(nic1.get_property('capacities').unit, 1)
         self.assertEqual(nic1.get_property('capacities').disk, 0)
         self.assertEqual(n1.components['nic1'].get_property('labels').ipv4, "192.168.1.12")
+        self.assertEqual(n1.components['nic1'].labels.ipv4, "192.168.1.12")
 
         # comparisons
         nic11 = n1.components['nic1']
@@ -121,6 +147,11 @@ class SliceTest(unittest.TestCase):
         # nodemap and unset
         n1.set_properties(node_map=('dead-beef-graph', 'dead-beef-node'))
         assert n1.get_property('node_map') == ('dead-beef-graph', 'dead-beef-node')
+        n1.unset_property('node_map')
+        assert n1.get_property('node_map') is None
+
+        n1.node_map = ('dead-beef-graph', 'dead-beef-node')
+        assert n1.node_map == ('dead-beef-graph', 'dead-beef-node')
         n1.unset_property('node_map')
         assert n1.get_property('node_map') is None
 
@@ -174,34 +205,41 @@ class SliceTest(unittest.TestCase):
 
         cap = f.Capacities()
         cap.set_fields(bw=50, unit=1)
-        nic1.set_properties(capacities=cap)
+        nic1.capacities=cap
         lab = f.Labels()
         lab.set_fields(ipv4="192.168.1.12")
-        nic1.set_properties(labels=lab)
+        nic1.labels = lab
         caphints = f.CapacityHints()
         caphints.set_fields(instance_type='blah')
-        n1.set_properties(capacity_hints=caphints)
+        #n1.set_properties(capacity_hints=caphints)
+        n1.capacity_hints = caphints
 
         # check capacities, hints labels on the graph
         n1p = self.topo.nodes['Node1']
         caphints1 = n1p.get_property('capacity_hints')
         assert(caphints.instance_type == caphints1.instance_type)
+        caphints1 = n1p.capacity_hints
+        assert (caphints.instance_type == caphints1.instance_type)
 
         #s1 = self.topo.add_network_service(name='s1', nstype=f.ServiceType.L2Bridge, interfaces=self.topo.interface_list)
 
         s1 = self.topo.add_network_service(name='s1', nstype=f.ServiceType.L2STS, interfaces=self.topo.interface_list)
 
+        self.assertEqual(s1.layer, f.Layer.L2)
+
         print(f'S1 has these interfaces: {s1.interface_list}')
         self.assertEqual(len(s1.interface_list), 6)
+        self.topo.validate()
 
         s1p = self.topo.network_services['s1']
 
-        print(f'S1p has these interfaces: {s1p.interface_list}')
+        print(f'S1 has these interfaces: {s1p.interface_list}')
 
         s1.disconnect_interface(interface=p1)
 
         print(f'S1 has these interfaces: {s1.interface_list}')
         self.assertEqual(len(s1.interface_list), 5)
+        self.topo.validate()
 
         self.topo.remove_network_service('s1')
 
@@ -215,6 +253,7 @@ class SliceTest(unittest.TestCase):
         print(f'Network services {self.topo.network_services}')
         self.assertEqual(len(self.topo.network_services), 5)
         self.topo.remove_network_service('s2')
+        self.topo.validate()
         self.assertEqual(len(self.topo.network_services), 4)
         n1.remove_component('nic1')
         self.assertEqual(len(self.topo.network_services), 3)
@@ -252,6 +291,58 @@ class SliceTest(unittest.TestCase):
         self.assertEqual(t1.graph_model.graph_id, self.topo.graph_model.graph_id)
         self.assertTrue('n1' in t1.nodes.keys())
         self.assertTrue('nic1' in t1.nodes['n1'].components.keys())
+
+    def testSerDes1(self):
+        # more thorough serialization-deserialization test
+        self.topo.add_node(name='n1', site='RENC')
+        self.topo.nodes['n1'].add_component(ctype=f.ComponentType.SharedNIC, model='ConnectX-6', name='nic1')
+        self.topo.add_node(name='n2', site='UKY')
+        self.topo.nodes['n2'].add_component(ctype=f.ComponentType.SharedNIC, model='ConnectX-6', name='nic1')
+        self.topo.add_network_service(name='s1', nstype=f.ServiceType.L2STS, interfaces=self.topo.interface_list)
+        gs = self.topo.serialize()
+        self.topo.graph_model.importer.delete_all_graphs()
+        t1 = f.ExperimentTopology(graph_string=gs)
+        self.assertEqual(t1.graph_model.graph_id, self.topo.graph_model.graph_id)
+        self.assertTrue('n1' in t1.nodes.keys())
+        self.assertTrue('nic1' in t1.nodes['n1'].components.keys())
+        print(f'LIST COMPONENTS of n1 {t1.nodes["n1"].components}')
+        print(f'LIST COMPONENTS of n2 {t1.nodes["n2"].components}')
+
+    def testSerDes2(self):
+        # more thorough serialization-deserialization test
+        topo = f.ExperimentTopology()
+        topo.add_node(name='n1', site='RENC')
+        cap = f.Capacities(core=1, unit=2)
+        topo.nodes['n1'].capacities = cap
+        topo.nodes['n1'].add_component(ctype=f.ComponentType.SharedNIC, model='ConnectX-6', name='nic1')
+        topo.add_node(name='n2', site='UKY')
+        topo.nodes['n2'].add_component(ctype=f.ComponentType.SharedNIC, model='ConnectX-6', name='nic2')
+        topo.add_network_service(name='s1', nstype=f.ServiceType.L2STS, interfaces=topo.interface_list)
+        slice_graph = topo.serialize()
+        print('\n\n\nINITIAL TOPO')
+        print(topo)
+
+        # Import it in the neo4j as ASM
+        generic_graph = self.n4j_imp.import_graph_from_string(graph_string=slice_graph)
+        asm_graph = Neo4jASMFactory.create(generic_graph)
+
+        # Serialize ASM
+        orch_graph = asm_graph.serialize_graph()
+
+        # Reload Experiment topology from serialized ASM
+        t1 = f.ExperimentTopology(graph_string=orch_graph)
+
+        print('\n\n\nRELOADED TOPO')
+        print(t1)
+        self.assertTrue('n1' in t1.nodes.keys())
+        self.assertTrue('nic1' in t1.nodes['n1'].components.keys())
+        cap1 = t1.nodes['n1'].capacities
+        self.assertEqual(cap1.core, 1)
+        self.assertEqual(cap1.unit, 2)
+
+        print(f'LIST COMPONENTS of n1 {t1.nodes["n1"].components}')
+        print(f'LIST COMPONENTS of n2 {t1.nodes["n2"].components}')
+        self.n4j_imp.delete_all_graphs()
 
     def testBasicOneSiteSlice(self):
         # create a basic slice and export to GraphML and JSON
