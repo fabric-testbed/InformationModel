75e4fd022948e49fd30d32a66d6dcdb958d8e3a3
Ilya Baldin <ibaldin@renci.org>
2021-10-15 22:11:53 -0400

    Added support to save and restore tags and MeasurementData

--- fim/graph/abc_property_graph_constants.py
+++ fim/graph/abc_property_graph_constants.py
@@ -67,13 +67,16 @@ class ABCPropertyGraphConstants(ABC):
     PROP_PATH_INFO = "PathInfo"
     PROP_CONTROLLER_URL = "ControllerURL"
     PROP_GATEWAY = "Gateway"
+    PROP_TAGS = "Tags"
+    PROP_MEAS_DATA = "MeasurementData"
     # these properties get validated to be valid JSON objects whenever someone validates the graph
     JSON_PROPERTY_NAMES = [PROP_LABELS, PROP_CAPACITIES, PROP_LABEL_DELEGATIONS,
                            PROP_CAPACITY_DELEGATIONS, PROP_LABEL_ALLOCATIONS,
                            PROP_CAPACITY_ALLOCATIONS, PROP_RESERVATION_INFO, PROP_STRUCTURAL_INFO,
-                           PROP_ERO, PROP_PATH_INFO, PROP_CAPACITY_HINTS, PROP_GATEWAY]
+                           PROP_ERO, PROP_PATH_INFO, PROP_CAPACITY_HINTS, PROP_GATEWAY,
+                           PROP_TAGS, PROP_MEAS_DATA]
     # these properties cannot be unset
-    NO_UNSET_PROPERTIES = [GRAPH_ID, NODE_ID, PROP_TYPE, PROP_CLASS]
+    NO_UNSET_PROPERTIES = [GRAPH_ID, NODE_ID, PROP_TYPE, PROP_CLASS, PROP_NAME]
 
     CLASS_NetworkNode = 'NetworkNode'
     CLASS_NetworkService = 'NetworkService'
