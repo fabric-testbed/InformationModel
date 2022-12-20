3d9d8c256edb00d21bfc280737146f0793c03f64
Ilya Baldin <ibaldin@renci.org>
2022-12-20 11:50:36 -0500

    Modified attribute_collector to look for FABNetvXExt services and added tests; This version used for testing Authz v1.4.0

--- test/attribute_collector_test.py
+++ test/attribute_collector_test.py
@@ -225,6 +225,7 @@ class AttributeCollectorTest(unittest.TestCase):
         s3 = t.add_port_mirror_service(name='pm2', from_interface_name='blahname2',
                                        to_interface=c4.interface_list[1])
 
+        t.validate()
         # this will take just about anything - an ASM, a Node, a NetworkService,
         # a sliver. So it can be called in AM, Broker or Orchestrator
         #
@@ -255,3 +256,54 @@ class AttributeCollectorTest(unittest.TestCase):
         req_dict = az.transform_to_pdp_request(as_json=False)
 
         print(f'6: JSON {req_json}')
+
+    def testCollector7(self):
+        # multi-site with FABNetv4Ext
+
+        az = ResourceAuthZAttributes()
+        print(az)
+        t = ExperimentTopology()
+        n1 = t.add_node(name='n1', site='RENC', capacities=Capacities(core=1, ram=10, disk=25))
+        c1 = n1.add_component(name='c1', model_type=ComponentModelType.SmartNIC_ConnectX_6)
+        c2 = n1.add_component(name='c2', model_type=ComponentModelType.SharedNIC_ConnectX_6)
+        n1.add_component(name='c3', model_type=ComponentModelType.NVME_P4510)
+        n2 = t.add_node(name='n2', site='UKY', capacities=Capacities(core=10, ram=10, disk=35))
+        c4 = n2.add_component(name='c4', model_type=ComponentModelType.SmartNIC_ConnectX_5)
+        s1 = t.add_network_service(name='s1', nstype=ServiceType.FABNetv4Ext,
+                                   interfaces=[c1.interface_list[0]])
+        s2 = t.add_network_service(name='s2', nstype=ServiceType.FABNetv4Ext,
+                                   interfaces=[c4.interface_list[0]])
+        # this sets site property on fabnet, which is a must
+        t.validate()
+
+        # this will take just about anything - an ASM, a Node, a NetworkService,
+        # a sliver. So it can be called in AM, Broker or Orchestrator
+        #
+        # However to get things like peering sites, facility sites, it is best to
+        # call it on the ASM (in Orchestrator)
+        az.collect_resource_attributes(source=t)
+        # generally you also want to set the lifetime (provided externally)
+        now = datetime.now(timezone.utc)
+        delta = timedelta(days=13, hours=11, minutes=7, seconds=4, milliseconds=10)
+        future = now + delta
+        az.set_lifetime(future)
+        print(az)
+        self.assertTrue('SmartNIC' in az.attributes[ResourceAuthZAttributes.RESOURCE_COMPONENT])
+        self.assertTrue('NVME' in az.attributes[ResourceAuthZAttributes.RESOURCE_COMPONENT])
+        self.assertTrue(25 in az.attributes[ResourceAuthZAttributes.RESOURCE_DISK])
+        self.assertEqual(az.attributes[ResourceAuthZAttributes.RESOURCE_SITE], ['RENC', 'UKY'])
+        self.assertTrue('P13DT11H7M4S' in az.attributes[ResourceAuthZAttributes.RESOURCE_LIFETIME])
+        self.assertEqual(az.attributes[ResourceAuthZAttributes.RESOURCE_TYPE], ["sliver"])
+        self.assertTrue('RENC' in az.attributes[ResourceAuthZAttributes.RESOURCE_FABNETV4_EXT])
+        self.assertTrue('UKY' in az.attributes[ResourceAuthZAttributes.RESOURCE_FABNETV4_EXT])
+
+        az.set_subject_attributes(subject_id="user@gmail.com", project=["Project1"],
+                                  project_tag=["Tag1", "Tag2"])
+        az.set_action("create")
+        az.set_resource_subject_and_project(subject_id="user@gmail.com", project="Project1")
+
+        # convert to full PDP request after adding attributes
+        req_json = az.transform_to_pdp_request()
+        req_dict = az.transform_to_pdp_request(as_json=False)
+
+        print(f'7: JSON {req_json}')
